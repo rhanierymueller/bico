af2842fa0e88872023feb55645b0ff406504c54f
Rhaniery Mueller <rhaniery.mueller@amplifilabs.com>
2023-01-14 15:48:03 -0300

    changes

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,3 +1,4 @@
+import {useNavigation} from '@react-navigation/native';
 import * as React from 'react';
 import {View, Text, TouchableOpacity, Image} from 'react-native';
 import {type TailwindFn} from 'twrnc';
@@ -6,33 +7,38 @@ type Props = {
 	tw: TailwindFn;
 };
 
-const Menu = ({tw}: Props) => (
-	<View style={tw`bg-white h-[8%]`}>
-		<View style={tw`flex-1 px-5 justify-center`}>
-			<View style={tw`flex-row items-center h-10`}>
-				<TouchableOpacity style={tw`flex-1 rounded-lg h-11 justify-center items-center`}
-					onPress={() => null}>
-					<Image style={tw`rounded h-5 w-5`} source = {require('C:/Users/Rhaniery/Documents/app-bico/bico/src/images/menu.png')} />
-					<Text style={tw`text-xs`}>Inicio</Text>
-				</TouchableOpacity>
-				<TouchableOpacity style={tw`flex-1 rounded-lg h-11 justify-center items-center`}
-					onPress={() => null}>
-					<Image style={tw`rounded h-5 w-5`} source = {require('C:/Users/Rhaniery/Documents/app-bico/bico/src/images/pesquisar.png')} />
-					<Text style={tw`text-xs`}>Pesquisar</Text>
-				</TouchableOpacity>
-				<TouchableOpacity style={tw`flex-1 rounded-lg h-11 justify-center items-center`}
-					onPress={() => null}>
-					<Image style={tw`rounded h-5 w-5`} source = {require('C:/Users/Rhaniery/Documents/app-bico/bico/src/images/add.png')} />
-					<Text style={tw`text-xs`}>Anunciar</Text>
-				</TouchableOpacity>
-				<TouchableOpacity style={tw`flex-1 rounded-lg h-11 justify-center items-center`}
-					onPress={() => null}>
-					<Image style={tw`rounded h-5 w-5`} source = {require('C:/Users/Rhaniery/Documents/app-bico/bico/src/images/profile.png')} />
-					<Text style={tw`text-xs`}>Profile</Text>
-				</TouchableOpacity>
+const Menu = ({tw}: Props) => {
+	const navigation = useNavigation();
+	return (
+		<View style={tw`bg-white h-[8%]`}>
+			<View style={tw`flex-1 px-5 justify-center`}>
+				<View style={tw`flex-row items-center h-10`}>
+					<TouchableOpacity style={tw`flex-1 rounded-lg h-11 justify-center items-center`}
+						onPress={() => null}>
+						<Image style={tw`rounded h-5 w-5`} source = {require('C:/Users/Rhaniery/Documents/app-bico/bico/src/images/menu.png')} />
+						<Text style={tw`text-xs`}>Inicio</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={tw`flex-1 rounded-lg h-11 justify-center items-center`}
+						onPress={() => null}>
+						<Image style={tw`rounded h-5 w-5`} source = {require('C:/Users/Rhaniery/Documents/app-bico/bico/src/images/pesquisar.png')} />
+						<Text style={tw`text-xs`}>Pesquisar</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={tw`flex-1 rounded-lg h-11 justify-center items-center`}
+						onPress={() => {
+							navigation.navigate('CadastroAnuncio');
+						}}>
+						<Image style={tw`rounded h-5 w-5`} source = {require('C:/Users/Rhaniery/Documents/app-bico/bico/src/images/add.png')} />
+						<Text style={tw`text-xs`}>Anunciar</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={tw`flex-1 rounded-lg h-11 justify-center items-center`}
+						onPress={() => null}>
+						<Image style={tw`rounded h-5 w-5`} source = {require('C:/Users/Rhaniery/Documents/app-bico/bico/src/images/profile.png')} />
+						<Text style={tw`text-xs`}>Profile</Text>
+					</TouchableOpacity>
+				</View>
 			</View>
 		</View>
-	</View>
-);
+	);
+};
 
 export default Menu;
